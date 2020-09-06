933d6a90097fe9c2967228fd0bb4f4c85b1224f1
Sanchita1108 <70022593+Sanchita1108@users.noreply.github.com>
2020-09-06 22:57:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,7 +18,10 @@ function draw()
     movingRect.y = World.mouseY;
     drawSprites();
 
-    if (movingRect.x - fixedRect.x < fixedRect.width/2 +  movingRect.width/2)
+    if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+        && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+        && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+        && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) 
     {
         fixedRect.shapeColor = "red";
         movingRect.shapeColor = "red";
